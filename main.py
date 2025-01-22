70d95d33ed3ed02185c16fdf34442be371f1dea3
Sphyrnus <sphyrnus@sphyrnus.com>
2025-01-22 10:55:21 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
